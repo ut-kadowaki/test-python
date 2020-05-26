6c47a2dd4997097bc70cba148ced5999d7a28d01
ut=kadowaki <kdwk@ut-c.co.jp>
2020-05-26 18:37:07 +0900

    test 1

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("Hello world")
-print("test_print")
+import tkinter
+
+root = tkinter.Tk()
+root.title("スカウト自動化")
+root.geometry("300x650")
+
+
+#ラベルフレームを配置
+url_frame = tkinter.LabelFrame(root,text="スプレットシート url",relief="ridge",bd=2)
+url_frame.pack(pady=10)
+
+url_box = tkinter.Entry(url_frame,width=30,bd=2)
+
+
+#url_box = tkinter.Entry(width=30)
+#url_box.place(x=40, y=30)
+#url_label = tkinter.Label(text="スプレットシート url")
+#url_label.place(x=40, y=6)
+#read_button = tkinter.Button(text="読込")
+#read_button.place(x=220, y=27)
+
+root.mainloop()

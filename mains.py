975893f1b5d889bcd747247e983639bc6db002c4
ut=kadowaki <kdwk@ut-c.co.jp>
2020-05-31 17:56:56 +0900

    .get

--- mains.py
+++ mains.py
@@ -8,12 +8,53 @@ class Tktest(Tk.Frame):
 
         #URL
         url = Tk.LabelFrame(self,text="スプレットシートURL")
+        ad_url=Tk.Entry(url,width=20)
+        ad_url.pack(side=Tk.LEFT)
+        readbut=Tk.Button(url,text="読込")
+        readbut.pack(side=Tk.LEFT,)
         url.pack(anchor=Tk.W, fill=Tk.X,ipadx=27)
-        self.txt=Tk.Entry(url,width=20)
-        self.txt.pack(side=Tk.LEFT)
-        self.but=Tk.Button(url,text="読込")
-        self.but.pack(side=Tk.LEFT,)
-        url.pack(anchor=Tk.W, fill=Tk.X)
+
+        #掲載番号
+        number = Tk.LabelFrame(self,text="掲載番号")
+        OptionList =[" ","1","2","3"]
+        variable = Tk.StringVar(number)
+        variable.set(OptionList[0])
+        opt = Tk.OptionMenu(number, variable, *OptionList)
+        opt.config(width = 28)
+        opt.pack()
+        number.pack(anchor=Tk.W, fill=Tk.X)
+
+        #クライアント名
+        clientNamebox = Tk.LabelFrame(self,text="クライアント名")
+        clientNamebox.pack(fill=Tk.X)
+        c_name=Tk.Entry(clientNamebox)
+        c_name.pack(fill=Tk.X)
+
+        #enログインID
+        log_id = Tk.LabelFrame(self,text="enログインID")
+        log_id.pack(fill=Tk.X)
+        en_id=Tk.Entry(log_id)
+        en_id.pack(fill=Tk.X)
+
+        #enログインPASSWORD
+        log_pass = Tk.LabelFrame(self,text="enログインPASSWORD")
+        log_pass.pack(fill=Tk.X)
+        en_pass=Tk.Entry(log_pass)
+        en_pass.pack(fill=Tk.X)
+
+        #通数
+        dev_stock = Tk.LabelFrame(self,text="通数")
+        dev_stock.pack(fill=Tk.X)
+        d_stock=Tk.Entry(dev_stock)
+        d_stock.pack(fill=Tk.X)
+
+        #掲載原稿の職種
+        job = Tk.LabelFrame(self,text="掲載原稿の職種")
+        job.pack(fill=Tk.X)
+        j_name=Tk.Entry(job)
+        j_name.pack(fill=Tk.X)
+
+        #検索条件
 
 
 
